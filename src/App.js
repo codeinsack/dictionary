d88ee82625f761789565c58b12e36ecd525ba05a
codeinsack <codeinsack@gmail.com>
2020-03-22 14:48:04 +0300

    add tabs component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Fragment } from 'react';
-import { Switch, Route, Link, withRouter } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
+
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import WordDefinition from "./pages/WordDefinition";
 import Dictionary from "./pages/Dictionary";
 import Practice from "./pages/Practice";
 import Progress from "./pages/Progress";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
+import Tabs from "./components/UI/Tabs/Tabs";
 
-function App({ location }) {
+function App() {
   return (
     <Fragment>
       <CssBaseline />
-      <Tabs
-        value={location.pathname}
-        indicatorColor="primary"
-        textColor="primary"
-      >
-        <Tab label="Word definition" component={Link} to="/" value="/" />
-        <Tab label="Dictionary" component={Link} to="/dictionary" value="/dictionary" />
-        <Tab label="Practice" component={Link} to="/practice" value="/practice" />
-        <Tab label="Progress" component={Link} to="/progress" value="/progress" />
-      </Tabs>
+      <Tabs />
       <Switch>
         <Route exact path="/" component={WordDefinition} />
         <Route path="/dictionary" component={Dictionary} />
@@ -33,4 +24,4 @@ function App({ location }) {
   );
 }
 
-export default withRouter(App);
+export default App;
