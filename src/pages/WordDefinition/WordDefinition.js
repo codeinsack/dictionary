9e1218a91e78ee95b753ab711babd4535964bd99
codeinsack <codeinsack@gmail.com>
2020-03-23 14:41:10 +0300

    split component

--- src/pages/WordDefinition/WordDefinition.js
+++ src/pages/WordDefinition/WordDefinition.js
@@ -6,16 +6,9 @@ import {
   Box,
   Button,
   TextField,
-  Card,
-  CardContent,
-  Typography,
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-  Chip,
 } from "@material-ui/core";
 
-import Carousel from "../../components/UI/Carousel/Carousel";
+import WordCard from "./WordCard";
 import { wordImagesActions } from "../../store/actions/wordImages"
 import { wordDefinitionActions } from "../../store/actions/wordDefinition"
 import wordImagesSelectors from "../../store/selectors/wordImages"
@@ -23,9 +16,8 @@ import wordDefinitionSelectors from "../../store/selectors/wordDefinition"
 
 const WordDefinition = () => {
   const [search, setSearch] = useState('');
-  const [expanded, setExpanded] = React.useState(0);
-  const wordImages = useSelector(wordImagesSelectors.getList)
   const wordDefinition = useSelector(wordDefinitionSelectors.getData)
+  const wordImages = useSelector(wordImagesSelectors.getList)
   const dispatch = useDispatch()
 
   const onSearchChange = (event) => {
@@ -37,10 +29,6 @@ const WordDefinition = () => {
     dispatch(wordImagesActions.fetchWordImages(search))
     dispatch(wordDefinitionActions.fetchWordDefinition(search))
   }
-
-  const onExpandedChange = panel => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false);
-  };
 
   return (
     <Wrapper>
@@ -61,43 +49,10 @@ const WordDefinition = () => {
         </Button>
       </Box>
       {wordDefinition && (
-        <Card>
-          <div>
-            <Carousel images={wordImages} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {wordDefinition.word}
-              </Typography>
-              {Object.keys(wordDefinition.meaning).map((key, index) => {
-                return (
-                  <ExpansionPanel
-                    key={key}
-                    square
-                    expanded={expanded === index}
-                    onChange={onExpandedChange(index)}
-                  >
-                    <ExpansionPanelSummary>
-                      <Typography>{key}</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                      <ul>
-                        {wordDefinition.meaning[key].map((variant, index) => (
-                          <li key={index}>
-                            <p>{variant.definition}</p>
-                            <p><i>{variant.example}</i></p>
-                            {variant.synonyms?.map(synonym => (
-                              <Chip key={synonym} label={synonym} />
-                            ))}
-                          </li>
-                        ))}
-                      </ul>
-                    </ExpansionPanelDetails>
-                  </ExpansionPanel>
-                )
-              })}
-            </CardContent>
-          </div>
-        </Card>
+        <WordCard
+          wordImages={wordImages}
+          wordDefinition={wordDefinition}
+        />
       )}
     </Wrapper>
   );
